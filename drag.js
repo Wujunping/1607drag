729dfff89a08fadfdc0d4fddd5f147ba1cb5ec30
zagcool <1658261070@qq.com>
2016-06-24 11:58:03 +0800

    last change

--- drag.js
+++ drag.js
@@ -1,7 +1,19 @@
 function drag(id){
 	var obj=document.getElementById(id);
 	var disX,disY;
-	obj.onmousedown=function(){
+	obj.onmousedown=function(ev){
+		ev=ev||event;
+		disX=ev.clientX-this.offsetLeft;
+		disY=ev.clientY-this.offsetTop;
+		
+		document.onmousemove=function(ev){
+			ev=ev||event;
+			obj.style.left=ev.clientX-disX+"px";
+			obj.style.top=ev.clientY-disY+"px";
+		}
+		document.onmouseup=function(){
+			document.onmousemove=document.onmouseup=null;
+		}
 		
 	}
 }
